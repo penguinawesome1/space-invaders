e2836202bafeda893504d80123fb0d55e5e479a1
Owen Colley <owen.h.colley@gmail.com>
2024-12-18 14:47:27 -0500

    hi

--- space-invaders.js
+++ space-invaders.js
@@ -1,104 +1,116 @@
-function startGame() {
-    const player = new Player(300, 30, "./images/repeating-snowflakes.svg", 10, 120);
-    const board = new Board(player);
-}
-
 class Board {
-    constructor(player) {
-        this.player = player;
+    constructor() {
         this.canvas = document.getElementById("game-board");
         this.context = this.canvas.getContext("2d");
-        this.frameNo = 0;
         this.alienList = [];
-        this.interval = setInterval(this.update(), 20);
+        this.bulletList = [];
+        this.initInputListeners();
+        this.loop = setInterval(this.update.bind(this), 32);
+    }
+
+    initInputListeners() {
+        document.addEventListener('keydown', (event) => {
+            switch(event.key.toUpperCase()) {
+                case "ARROWLEFT":
+                case "A": player.moveLeft(); break;
+                case "ARROWRIGHT":
+                case "D": player.moveRight(); break;
+                case "ARROWUP":
+                case "W": this.shootBullet(); break;
+            }
+        });
+        
+        document.addEventListener('keyup', (event) => {
+            switch(event.key.toUpperCase()) {
+                case "ARROWLEFT":
+                case "A": player.stopLeft(); break;
+                case "ARROWRIGHT":
+                case "D": player.stopRight(); break;
+            }
+        });
+    }
+
+    shootBullet() {
+        const url = "./images/cannon.png";
+        const bullet = new Bullet(url, 10, 10, player.x + (player.width / 2), player.y - 15, -10);
+        bullet.draw();
+        this.bulletList.push(bullet);
+    }
+
+    spawnAlien() {
+        const url = "./images/cannon.png";
+        const x = this.getRandomInt(this.canvas.width);
+        const alien = new Alien(url, 30, 30, x, 3);
+        alien.draw();
+        this.alienList.push(alien);
     }
   
     update() {
-        this.clear();
-        this.frameNo++;
-            
-        if (this.alienList) {
-            for (let i = 0; i < this.alienList.length; i += 1) {
-                if (this.player.crashWith(this.alienList[i])) {
-                    // GAME OVER
-                    return;
-                }
+        for (let i = 0; i < this.alienList.length; i += 1) {
+            if (player.crashWith(this.alienList[i])) {
+                console.log("game over!");
+                clearInterval(board.loop);
+                return;
             }
         }
 
-        if (this.frameNo == 1 || this.everyInterval(150)) {
-            const alien = new Alien(10, 10, "./images/canon.svg", this.canvas.width, 0);
-            alien.speedY = 10;
-            this.alienList.push(alien);
-        }
-
+        this.clear();
         for (let i = 0; i < this.alienList.length; i += 1) {
             this.alienList[i].newPos();
-            this.alienList[i].update();
+            this.alienList[i].draw();
+            if (player.crashWith(this.alienList[i])) {
+                console.log("game over!");
+                clearInterval(board.loop);
+                return;
+            }
+            for (let a = 0; a < this.bulletList.length; a += 1) {
+                if (this.bulletList[a].crashWith(this.alienList[i])) {
+                    board.alienList.splice(board.alienList.indexOf(this), 1);
+                    board.bulletList.splice(board.bulletList.indexOf(this), 1);
+                }
+            }
+        }
+        for (let i = 0; i < this.bulletList.length; i += 1) {
+            this.bulletList[i].newPos();
+            this.bulletList[i].draw();
+            if (player.crashWith(this.bulletList[i])) {
+                console.log("game over!");
+                clearInterval(board.loop);
+                return;
+            }
         }
         player.newPos();
-        player.update();
+        player.draw();
+
+        if (this.getRandomInt(80) === 0) {
+            this.spawnAlien();
+        }
+    }
+
+    getRandomInt(n) {
+        return Math.floor(Math.random() * n);
     }
 
     clear() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
-
-    everyInterval(n) {
-        return (this.frameNo / n) % 1 == 0;
-    }
 }
 
 class Component {
-    constructor(width, height, x, y, imageUrl) {      
+    constructor(width, height, x, y, imageUrl) {
+        this.canvas = document.getElementById("game-board");
+        this.context = this.canvas.getContext("2d");
         this.width = width;
         this.height = height;
         this.x = x;
         this.y = y;
-        
-        let image = new Image();
-	    image.src = imageUrl;
-	    image.onload = () => {
-            this.image = image;
-            this.update();
-        }
+        this.image = new Image();
+	    this.image.src = imageUrl;
+	    this.image.onload = () => this.draw();
     }
     
-    update() {
+    draw() {
         this.context.drawImage(this.image, this.x, this.y, this.width, this.height);
-    }
-}
-
-class Alien extends Component {
-    constructor(width = 30, height = 30, x = 30, imageUrl, speedY = 10) {
-        const y = 0;
-        super(width, height, x, y, imageUrl);
-        this.speedY = speedY;
-        this.canvas = document.getElementById("game-board");
-        this.context = this.canvas.getContext("2d");
-    }
-
-    newPos() {
-        this.y += this.speedY;
-        this.hitBottom();
-    }
-
-    hitBottom() {
-        const rockbottom = this.canvas.height - this.height;
-        if (this.y > rockbottom + this.height) {
-            alienList.splice(alienList.indexOf(this), 1);
-        }
-    }
-}
-
-class Player extends Component {
-    constructor(imageUrl) {
-        const width = 30;
-        const height = 30;
-        const x = 0;
-        const y = 0;
-        super(width, height, x, y, imageUrl);
-        this.speedX = 0;
     }
 
     crashWith(otherObj) {
@@ -112,28 +124,98 @@ class Player extends Component {
         const otherBottom = otherObj.y + (otherObj.height);
         return !((myBottom < otherTop) || (myTop > otherBottom) || (myRight < otherLeft) || (myLeft > otherRight));
     }
+}
+
+class Player extends Component {
+    constructor() {
+        const canvas = document.getElementById("game-board");
+        const width = 180;
+        const height = 40;
+        const x = 100;
+        const y = canvas.height - 100;
+        const url = "./images/cannon.png";
+        super(width, height, x, y, url);
+        this.speedX = 0;
+    }
 
     newPos() {
-        this.x += this.speedX;
+        const onScreen = this.x + this.speedX > 0 && this.x + this.speedX + this.width < this.canvas.width;
+        if (onScreen) {
+            this.x += this.speedX;
+        }
+    }
+
+    moveLeft() {
+        this.speedX = -5;
+    }
+
+    moveRight() {
+        this.speedX = 5;
+    }
+
+    stopLeft() {
+        if (this.speedX < 0) {
+            this.speedX = 0;
+        }
+    }
+
+    stopRight() {
+        if (this.speedX > 0) {
+            this.speedX = 0;
+        }
     }
 }
 
-document.addEventListener('keydown', (event) => {
-    switch (event.key) {
-        case 'w': break;
-        case 'a': player.speedX -= 10; break;
-        case 's': break;
-        case 'd': player.speedX += 10; break;
+class Alien extends Component {
+    constructor(imageUrl, width = 30, height = 30, x = 150, speedY = 4) {
+        const y = 0;
+        super(width, height, x, y, imageUrl);
+        this.speedY = speedY;
     }
+
+    newPos() {
+        this.y += this.speedY;
+        this.hitBottom();
+    }
+
+    hitBottom() {
+        if (this.y > this.canvas.height) {
+            board.alienList.splice(board.alienList.indexOf(this), 1);
+        }
+    }
+}
+
+class Bullet extends Component {
+    constructor(imageUrl, width = 10, height = 10, x = 200, y = 200, speedY = -10) {
+        super(width, height, x, y, imageUrl);
+        this.speedY = speedY;
+    }
+
+    newPos() {
+        this.y += this.speedY;
+        this.hitTopOrBottom();
+    }
+
+    hitTopOrBottom() {
+        if (this.y > this.canvas.height || this.y < 0) {
+            board.bulletList.splice(board.bulletList.indexOf(this), 1);
+        }
+    }
+}
+
+const player = new Player();
+const board = new Board();
+
+const dialog = document.getElementById("pause-menu");
+
+document.getElementById("pause-button").addEventListener("click", () => {
+    dialog.showModal();
+    dialog.classList.remove("hidden");
+    clearInterval(board.loop);
 });
 
-document.addEventListener('keyup', (event) => {
-    switch (event.key) {
-        case 'w': break;
-        case 'a': player.speedX += 10; break;
-        case 's': break;
-        case 'd': player.speedX -= 10; break;
-    }
+document.getElementById("unpause").addEventListener("click", () => {
+    dialog.close();
+    dialog.classList.add("hidden");
+    board.loop = setInterval(board.update.bind(board), 32);
 });
-
-startGame();
